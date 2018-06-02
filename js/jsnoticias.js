72a714b53743afb4df6fefc720f46901d5fe694c
daniochoamesa <32293791+daniochoamesa@users.noreply.github.com>
2018-06-02 22:42:28 +0200

    Update jsnoticias.js

--- js/jsnoticias.js
+++ js/jsnoticias.js
@@ -1,16 +1,1 @@
-//variables
-//funciones
-
-var contador=1;
-var temporizador = setInterval(myTimer, 2000);
-
-var c=[];
-c[0]="img/publi1.png";
-c[1]="img/publi2.jpg";
-c[2]="img/publi3.jpg";
-
-function myTimer() {
-	contador++;
-	$("#ppp").attr('src',c[contador-1]);;
-	if (contador==3) contador=0;
-}
+var contador=1,temporizador=setInterval(myTimer,2e3),c=[];function myTimer(){contador++,$("#ppp").attr("src",c[contador-1]),3==contador&&(contador=0)}c[0]="img/publi1.png",c[1]="img/publi2.jpg",c[2]="img/publi3.jpg";
